d88f67ee23b12c2f044f8f05c319d9590c6bfe01
Hack Reactor Students <github@hackreactor.com>
2013-07-11 20:25:44 -0700

    finished chat schema in MySQL

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -2,10 +2,34 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
- /* Describe your table here.*/
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT primary key,
+  name varchar(30)
 );
 
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT primary key,
+  username varchar(30)
+);
+
+CREATE TABLE messages (
+  id INT NOT NULL AUTO_INCREMENT primary key,
+  content varchar(140),
+  created timestamp default '0000-00-00 00:00:00',
+  updated timestamp default now() on update now(),
+  user_id INT NOT NULL,
+  room_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
+
+CREATE TABLE friends (
+  id INT NOT NULL AUTO_INCREMENT primary key,
+  user_id1 INT NOT NULL,
+  user_id2 INT NOT NULL,
+  FOREIGN KEY (user_id1) REFERENCES users(id),
+  FOREIGN KEY (user_id2) REFERENCES users(id)
+);
 /* You can also create more tables, if you need them... */
 
 /*  Execute this file from the command line by typing:
